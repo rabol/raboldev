d2eb06ef17b20b5c7935acf61187a3e94638e4bd
rabol <steen@rabol.com>
2024-08-29 07:21:48 +0200

    update

--- deploy.php
+++ deploy.php
@@ -6,13 +6,13 @@ require 'contrib/npm.php';
 // Config
 
 set('repository', 'git@github.com:rabol/raboldev.git');
-set('writable_mode', 'chown');
+//set('writable_mode', 'chown');
 set('writable_recursive', 'true');
-set('writable_chmod_mode', '0775');
+set('writable_chmod_mode', '0755');
 set('writable_use_sudo','true');
 add('shared_files', []);
 add('shared_dirs', []);
-add('writable_dirs', ['content','users','bootstrap/cache']);
+//add('writable_dirs', ['content','users','bootstrap/cache']);
 
 // Hosts
 
@@ -26,7 +26,16 @@ task('npm:build', function () {
     run("cd {{release_path}} && {{bin/npm}} run build");
 });
 
+desc('Set Statamic parmission');
+task('statamic:permission', function () {
+    run("sudo chown -R www-data:www-data {{deploy_path}}/current/storage");
+    run("sudo chown -R www-data:www-data {{deploy_path}}/current/content");
+    run("sudo chown -R www-data:www-data {{deploy_path}}/current/bootstrap/cache");
+    run("sudo chown -R www-data:www-data {{deploy_path}}/current/public");
+});
+
 // Hooks
 before('deploy:publish','npm:install');
 after('npm:install', 'npm:build');
 after('deploy:failed', 'deploy:unlock');
+after('deploy:publish', 'statamic:permission');
